68dcb5e8002959e4706bb83abbf337a4c20a1910
Santiago <titoalfaro@gmail.com>
2014-02-10 16:10:30 -0500

    Commit

--- src/mit/edu/obmg/tempsensing/TempSensingMain.java
+++ src/mit/edu/obmg/tempsensing/TempSensingMain.java
@@ -15,9 +15,6 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.SeekBar;
-import android.widget.ToggleButton;
-import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 public class TempSensingMain extends IOIOActivity implements OnClickListener{
