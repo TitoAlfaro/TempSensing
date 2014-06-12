5ebf4b8d4f9f9debfbb1c03a58f54cae49ac56d7
TitoAlfaro <titoalfaro@gmail.com>
2014-06-12 14:10:23 -0400

    before merge with Diana

--- gen/mit/edu/obmg/tempsensing/R.java
+++ gen/mit/edu/obmg/tempsensing/R.java
@@ -17,46 +17,46 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Button01Minus=0x7f090002;
-        public static final int Button01Plus=0x7f090003;
-        public static final int Button02Minus=0x7f090007;
-        public static final int Button02Plus=0x7f090008;
-        public static final int Button03Minus=0x7f09000c;
-        public static final int Button03Plus=0x7f09000d;
-        public static final int ValueMulti01=0x7f090004;
-        public static final int ValueMulti02=0x7f090009;
-        public static final int ValueMulti03=0x7f09000e;
-        public static final int action_settings=0x7f09000f;
-        public static final int tempF1=0x7f090000;
-        public static final int tempF2=0x7f090005;
-        public static final int tempF3=0x7f09000a;
-        public static final int tempP1=0x7f090001;
-        public static final int tempP2=0x7f090006;
-        public static final int tempP3=0x7f09000b;
+        public static final int Button01Minus=0x7f080002;
+        public static final int Button01Plus=0x7f080003;
+        public static final int Button02Minus=0x7f080007;
+        public static final int Button02Plus=0x7f080008;
+        public static final int Button03Minus=0x7f08000c;
+        public static final int Button03Plus=0x7f08000d;
+        public static final int ValueMulti01=0x7f080004;
+        public static final int ValueMulti02=0x7f080009;
+        public static final int ValueMulti03=0x7f08000e;
+        public static final int action_settings=0x7f08000f;
+        public static final int tempF1=0x7f080000;
+        public static final int tempF2=0x7f080005;
+        public static final int tempF3=0x7f08000a;
+        public static final int tempP1=0x7f080001;
+        public static final int tempP2=0x7f080006;
+        public static final int tempP3=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_temp_sensing_main=0x7f030000;
     }
     public static final class menu {
-        public static final int temp_sensing_main=0x7f080000;
+        public static final int temp_sensing_main=0x7f070000;
     }
     public static final class string {
-        public static final int MinusSign=0x7f060008;
-        public static final int PlusSign=0x7f060007;
-        public static final int TempReadingF=0x7f060003;
-        public static final int TempReadingP=0x7f060002;
-        public static final int ValueMulti01=0x7f060004;
-        public static final int ValueMulti02=0x7f060005;
-        public static final int ValueMulti03=0x7f060006;
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
+        public static final int MinusSign=0x7f050008;
+        public static final int PlusSign=0x7f050007;
+        public static final int TempReadingF=0x7f050003;
+        public static final int TempReadingP=0x7f050002;
+        public static final int ValueMulti01=0x7f050004;
+        public static final int ValueMulti02=0x7f050005;
+        public static final int ValueMulti03=0x7f050006;
+        public static final int action_settings=0x7f050001;
+        public static final int app_name=0x7f050000;
     }
     public static final class style {
         /** 
@@ -80,13 +80,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
-    }
-    public static final class xml {
-        public static final int accessory_filter=0x7f040000;
+        public static final int AppTheme=0x7f060001;
     }
 }
